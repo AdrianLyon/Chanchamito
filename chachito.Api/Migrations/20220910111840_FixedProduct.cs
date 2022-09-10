cac23c8123552be7cdfdf09b0e100db86429853b
AdriaanLeon <leon.adriand@gmail.com>
2022-09-10 06:55:58 -0500

    fixedModelsRelations

---
+++ chachito.Api/Migrations/20220910111840_FixedProduct.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chachito.Api.Migrations
+{
+    public partial class FixedProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCategory_Product_ProductId",
+                table: "ProductCategory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductCategory_ProductId",
+                table: "ProductCategory");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "ProductCategory");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductCategoryId",
+                table: "Product",
+                column: "ProductCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_ProductCategoryId",
+                table: "Product");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "ProductCategory",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategory_ProductId",
+                table: "ProductCategory",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCategory_Product_ProductId",
+                table: "ProductCategory",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id");
+        }
+    }
+}
--- chachito.Api/Migrations/20220910113757_FixedOrderDetail.Designer.cs
+++ chachito.Api/Migrations/20220910113757_FixedOrderDetail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using chachito.Api.Data;
 
@@ -11,9 +12,10 @@ using chachito.Api.Data;
 namespace chachito.Api.Migrations
 {
     [DbContext(typeof(ChanchitoDbContext))]
-    partial class ChanchitoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220910113757_FixedOrderDetail")]
+    partial class FixedOrderDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
